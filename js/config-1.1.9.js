47a52dcced5169575dd2bb6dd1b61bf1079f5189
earthspay <a68631831@gmail.com>
2019-04-13 19:58:39 +1100

    Release 1.1.9

---
+++ js/config-1.1.9.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular.module('web').constant('appConfig', {
+        apiDomain: 'https://earthsnodes.earths.ga',
+        dataServiceBaseUrl: 'https://api.earths.ga/v0',
+        blockchainName: 'Mainnet',
+        title: 'Earths Explorer',
+        nodes: [{
+            url: 'https://earthsnodes.earths.ga',
+            maintainer: 'Earths'
+        }],
+        peerExplorer: {
+            url: 'https://testnet.explorer.earths.ga',
+            title: 'TESTNET Explorer'
+        },
+        wallet: {
+            url: 'https://lc.earths.ga',
+            title: 'Wallet'
+        }
+    });
+
+    angular.module('web').constant('constants.network', {
+        NETWORK_NAME: 'mainnet', // 'devnet', 'testnet', 'mainnet'
+        ADDRESS_VERSION: 1,
+        NETWORK_CODE: 'W',
+        INITIAL_NONCE: 0
+    });
+})();
